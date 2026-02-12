bc8628609c6b3258267526f2895d857bb3451900
sndjy1986 <sndjy1986@gmail.com>
2026-02-12 07:27:14 -0500

    Add users table to schema.sql

--- api/schema.sql
+++ api/schema.sql
@@ -1,5 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+  id TEXT PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password_hash TEXT NOT NULL,
+  salt TEXT NOT NULL,
+  iterations INTEGER NOT NULL,
+  created_at INTEGER NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS calendars (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
-  color TEXT NOT NULL
+  color TEXT NOT NULL,
+  created_by TEXT NOT NULL,
+  created_at INTEGER NOT NULL,
+  FOREIGN KEY(created_by) REFERENCES users(id)
 );
